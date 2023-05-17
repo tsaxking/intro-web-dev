2f5f914323728d2727f92e91a0e5df6eba20b5c7
github-account-i-guess <nathaniel.m.laursen@gmail.com>
2023-05-16 20:48:39 -0600

    added a walkthrough for the project

---
+++ lessons/5 - Javascript/5.4 - Arrays/project-comments.js
@@ -0,0 +1,46 @@
+// Create a new function that generates 
+// a doubled array that is just filled with blank strings
+    // You can use new Array(length) to create an array of a certain length
+    // You can then use fill to fill the array with a value
+    // (Note that in order to use map you have to fill an array for some reason)
+    // Also, note that filling arrays can be goofy because of dependencies
+
+
+// Create a function that will take in a doubled array,
+// combine it and then log the combined array
+// You can use console.clear() in order to remove the previous drawings
+
+/*
+    Create a function named animate that 
+    will take in an array of { 
+        x: number,
+        y: number
+    } objects
+
+    Then use set interval to loop through the array 
+    and call your draw function from earlier on a blank doubled array that has been edited
+*/
+    // You can use a variable to store where you are within the array 
+
+    // Then you can use setInterval to draw things each frame
+        // You can use the first function you created to generate
+        // A blank doubled array
+
+        // Then you can take a coordinate from the coordinate array
+        // You can edit a certain place (based of the coordinate) in the array
+        // and change that place's value to "O" or some other string
+
+        // Finally you can call your draw function on the doubled array to log it
+
+        // Also remember to increment your variable 
+        // that is storing where you are in the array
+
+/* 
+    Create an array of coordinates in the form:
+        { 
+            x: number,
+            y: number
+        }
+*/
+
+// Then call your animate function on the array!
