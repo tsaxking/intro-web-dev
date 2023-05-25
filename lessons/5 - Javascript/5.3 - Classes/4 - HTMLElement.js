36c2dd4366baac51a1428daf30a84bdc407de0b5
github-account-i-guess <nathaniel.m.laursen@gmail.com>
2023-05-25 15:37:15 -0600

    did the rest of the lesson stuff for the classes lesson

--- lessons/5 - Javascript/5.3 - Classes/4 - HTMLElement.js
+++
@@ -1,5 +0,0 @@
-// Note that this code will only work in dev tools because
-// node is server side and so document isn't defined in node
-
-const el = document.createElement("div");
-console.log(el.style);
