1f246407f94b7daa7ba94b8c398b522d217c2db5
github-account-i-guess <nathaniel.m.laursen@gmail.com>
2023-05-25 14:04:13 -0600

    added try catch explanation and started on clases

---
+++ lessons/5 - Javascript/5.2 - Scopes and Stuff/9 - try catch.js
@@ -0,0 +1,12 @@
+// A try catch lets you handle errors
+// Instead of errors causing the code to stop you can put a catch statement
+// When an error is thrown within the try, it will activate the catch
+let probablyFunction;
+
+try {
+    probablyFunction(1);
+} catch (e) {
+    console.error(e);
+}
+
+console.log("code still running!!!")
