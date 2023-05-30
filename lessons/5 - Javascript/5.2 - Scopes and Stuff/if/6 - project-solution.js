d056c9b6f48b3397f18fdf00fcb8e2bc974a70fa
github-account-i-guess <nathaniel.m.laursen@gmail.com>
2023-05-30 15:31:17 -0600

    commeted through a project solution

--- lessons/5 - Javascript/5.2 - Scopes and Stuff/if/6 - project-solution.js
+++ lessons/5 - Javascript/5.2 - Scopes and Stuff/if/6 - project-solution.js
@@ -4,5 +4,5 @@ const rand = Math.random();
 // Getting a boolean to represent whether the random number is above 0.5
 const heads = rand >= 0.5;
 
-// Loging heads if the boolean is true and tails otherwise
+// Logging heads if the boolean is true and tails otherwise
 console.log(heads ? "heads" : "tails");
