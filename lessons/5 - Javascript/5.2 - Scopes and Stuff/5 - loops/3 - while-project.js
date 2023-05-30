2d28b53cd1da6fcdebae1ca2de59b68d3e4ae15b
github-account-i-guess <nathaniel.m.laursen@gmail.com>
2023-05-30 16:52:56 -0600

    Added a while loop project

---
+++ lessons/5 - Javascript/5.2 - Scopes and Stuff/5 - loops/3 - while-project.js
@@ -0,0 +1,2 @@
+// Create a script that uses while loops to log the number 1 once, 2 twice, 3 thrice, all the way up to 5
+// Hint: You may have to use more than one while loop
---
+++ lessons/5 - Javascript/5.2 - Scopes and Stuff/5 - loops/7 - for-project-solution.js
@@ -0,0 +1,5 @@
+for (i = 1; i < 6; i++) {
+    for (j = 0; j < i; j ++) {
+        console.log(i);
+    }
+}
